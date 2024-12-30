acb4cb2ad1f0d8cc0575915a80a2d0849935597d
Nander Wever <nander.wever@slf.ch>
2024-12-30 16:41:39 +0100

    Improvements to comparison with measured density profiles
    1) Case insensitve comparison to deal with inconsistencies in data files
    2) Including both tundra and wet 0 sites, to show variability in observations

--- simulations/postprocess/main_extract_profiles.sh
+++ simulations/postprocess/main_extract_profiles.sh
@@ -1,4 +1,4 @@
-for d in $(cat ../../download/BylotDensity201* | fgrep TUNDRA | awk '{printf "%04d-05-%02d\n", 2000+substr($1,8,2), substr($1,1,2)}' | uniq); do
+for d in $(cat ../../download/BylotDensity201* | fgrep -i TUNDRA | awk '{printf "%04d-05-%02d\n", 2000+substr($1,8,2), substr($1,1,2)}' | uniq); do
     echo ${d}
     for profile in ../output/BYL*pro
     do
