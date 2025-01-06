6254b7abe7c6655ef529c599bce48dd13f9cb35c
Nander Wever <nander.wever@slf.ch>
2025-01-06 09:01:42 +0100

    Adding a workflow script for the simulations

---
+++ simulations/simulation_workflow.sh
@@ -0,0 +1,8 @@
+bash prepare_snowpack.sh
+parallel < to_exec.lst
+
+bash main_postprocess.sh
+parallel < main_postprocess_to_exec.lst
+
+gnuplot compare_density.gnu
+gnuplot compare_density_fetch_length.gnu
