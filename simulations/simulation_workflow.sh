39fa64f813192ef1d504c053ba525323a670a6a5
Nander Wever <nander.wever@slf.ch>
2025-01-21 11:31:37 +0100

    Since postprocessing can cause some high RAM usage, limit the number of parallel processes

--- simulations/simulation_workflow.sh
+++ simulations/simulation_workflow.sh
@@ -4,7 +4,7 @@ parallel < to_exec.lst
 bash main_extract_profiles.sh
 
 bash main_postprocess.sh
-parallel < main_postprocess_to_exec.lst
+parallel -j4 < main_postprocess_to_exec.lst
 
 gnuplot compare_density.gnu
 gnuplot compare_density_fetch_length.gnu
