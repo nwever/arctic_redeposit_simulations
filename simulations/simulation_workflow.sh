3047221a48833dd4561119c77e8c0e725b531815
Nander Wever <nander.wever@slf.ch>
2025-01-23 18:13:27 +0100

    Setting PATH correctly in the simulation_workflow script, now that snowpack is a git submodule. Also adding gnuplot command to make the sensitivity plot for the averaging depth experiments.

--- simulations/simulation_workflow.sh
+++ simulations/simulation_workflow.sh
@@ -1,3 +1,5 @@
+export PATH=./usr/bin:${PATH}
+
 bash prepare_snowpack.sh
 parallel < to_exec.lst
 
@@ -8,3 +10,4 @@ parallel -j4 < main_postprocess_to_exec.lst
 
 gnuplot compare_density.gnu
 gnuplot compare_density_fetch_length.gnu
+gnuplot compare_density_avg_depth.gnu
